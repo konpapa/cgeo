e48bd969ea51ceed224bdaea64aa4e17034ace4f
konpapa <diadiaki19@hotmail.com>
2015-06-06 20:39:40 +0300

    Discard changes

--- main/src/cgeo/geocaching/DialogWaypointActivity.java
+++ main/src/cgeo/geocaching/DialogWaypointActivity.java
@@ -29,6 +29,7 @@ public class DialogWaypointActivity extends DialogFragment {
         alertDialog.setNegativeButton("Discard", new DialogInterface.OnClickListener() {
         public void onClick(DialogInterface dialog, int which) {
         	// User pressed Discard button. Write Logic Here
+        	EditWaypointActivityEditWaypointActivity.finish();
                 Toast.makeText(getApplicationContext(), "Your changes have not been saved", Toast.LENGTH_SHORT).show();
                 }
         });
